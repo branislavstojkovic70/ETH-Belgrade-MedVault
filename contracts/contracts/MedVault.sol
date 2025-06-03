8672e2605358dd01675b15a978526e46d16fae92
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2025-06-03 13:18:31 +0200

    Added functions AccessGrant and AcessFile
    Grant access function - give access token to doctor
    AcessFile - is used to access granted file

--- contracts/contracts/MedVault.sol
+++ contracts/contracts/MedVault.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.30;
 
 import "@oasisprotocol/sapphire-contracts/contracts/Sapphire.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 struct FileInfo {
     uint256 fileId;
@@ -21,6 +22,7 @@ struct DoctorAccess {
 contract MedVault {
     mapping(uint256 => FileInfo) private files;
     mapping(address => FileInfo[]) private ownerFiles;
+    mapping(string => DoctorAccess) private doctorAccess;
 
     constructor() { }
 
@@ -44,5 +46,32 @@ contract MedVault {
         files[fileId] = fileInfo;
         ownerFiles[msg.sender].push(fileInfo);
     }
+
+    function grantAccess(
+        address doctor,
+        uint256 fileId,
+        uint256 duration
+    ) external {
+        FileInfo memory file = files[fileId];
+        require(file.owner == msg.sender, "Unauthorized");
+
+        bytes memory rnd = Sapphire.randomBytes(32, "");
+        string memory accessToken = Strings.toHexString(uint256(keccak256(rnd)));
+
+        doctorAccess[accessToken] = DoctorAccess({
+            doctor: doctor,
+            fileId: fileId,
+            endDate: block.timestamp + duration * 60
+        });
+
+    }
+
+    function accessFile(
+        string memory accessToken
+    ) external view returns (FileInfo memory) {
+        DoctorAccess memory access = doctorAccess[accessToken];
+        return files[access.fileId];
+    }
+
 
 }
