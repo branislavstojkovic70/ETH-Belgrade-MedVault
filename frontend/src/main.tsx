02278c964f1ad2ed476787b8de9957f874183af4
Милош Чутурић <miloscuturic8@gmail.com>
2025-06-03 13:56:34 +0200

    Setup color theme and added login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,65 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/login.tsx";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#092147",
+			light: "#174384",
+			contrastText: "#F5F5F5",
+		},
+		secondary: {
+			main: "#F5F5F5",
+			contrastText: "#092147",
+		},
+		error: {
+			main: "#DD3636",
+		},
+		success: {
+			main: "#66FF77",
+		},
+		background: {
+			default: "#F5F5F5",
+		},
+	},
+	typography: {
+		allVariants: {
+			color: "#092147",
+			fontFamily: "Poppins",
+		},
+	},
+});
+
+const router = createBrowserRouter([
+	{
+		path: "/login",
+		element: <Login />,
+	},
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<ThemeProvider theme={theme}>
+		<CssBaseline />
+		<RouterProvider router={router} />
+		<Toaster
+			position="bottom-center"
+			toastOptions={{
+				success: {
+					style: {
+						background: theme.palette.success.main,
+					},
+				},
+				error: {
+					style: {
+						background: theme.palette.error.main,
+						color: "#F5F5F5",
+					},
+				},
+			}}
+		/>
+	</ThemeProvider>
+);
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,70 @@
+import { Box, Button, Typography } from "@mui/material";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+	const handleLogin = async () => {
+		toast.success("Login works.");
+	};
+
+	return (
+		<Box
+			sx={{
+				height: "100vh",
+				width: "100vw",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				gap: 4,
+				padding: 4,
+				backgroundColor: "#F5F5F5",
+			}}
+		>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					gap: 2,
+				}}
+			>
+				<Typography
+					sx={{
+						fontSize: "34px",
+						fontWeight: 600,
+						textAlign: "center",
+					}}
+				>
+					Welcome to MedVault.
+				</Typography>
+
+				<Typography
+					sx={{
+						fontFamily: "Poppins, sans-serif",
+						fontSize: "30px",
+						fontWeight: 500,
+						color: "#092147",
+						textAlign: "center",
+					}}
+				>
+					Secure. Private. Decentralized Health.
+				</Typography>
+			</Box>
+
+			<Button
+                variant="contained"
+				sx={{
+					fontSize: "20px",
+					textTransform: "capitalize",
+					paddingY: 1.5,
+					width: "100%",
+					maxWidth: "400px",
+				}}
+                onClick={handleLogin}
+            >
+                Continue with MetaMask
+			</Button>
+		</Box>
+	);
+}
